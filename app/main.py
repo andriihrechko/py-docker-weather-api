dab2576388f1ab1709a9e1346ef3680794530841
andriihrechko <andriihrechko250703@gmail.com>
2026-06-10 00:02:41 +0300

    implement getting current weather from Paris

--- app/main.py
+++ app/main.py
@@ -1,7 +1,30 @@
-def get_weather() -> None:
-    # write your code here
-    pass
+import os
+
+import requests
+
+WEATHER_API_KEY = os.getenv("API_KEY")
+BASE_URL = "https://api.weatherapi.com/v1/current.json"
+LOCATION = "Paris"
+
+
+def get_weather() -> str:
+    response = requests.get(f"{BASE_URL}?q={LOCATION}&key={WEATHER_API_KEY}")
+    if response.status_code == 200:
+        data = response.json()
+        location = data.get("location", {})
+        current = data.get("current", {})
+
+        country = location.get("country")
+        localtime = location.get("localtime")
+        celsius = current.get("temp_c")
+        condition = current.get("condition", {}).get("text")
+
+        return (f"{LOCATION}/{country} {localtime} "
+                f"Weather: {celsius} Celsius, {condition}")
+    else:
+        return f"Error {response.status_code}"
 
 
 if __name__ == "__main__":
-    get_weather()
+    print(f"Performing request to Weather API for city {LOCATION}")
+    print(get_weather())
